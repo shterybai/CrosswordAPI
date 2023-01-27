751cde90ae5cc26f9b447e2a7cb466d9a92d288e
shterybai <andrewkennan2000@gmail.com>
2023-01-27 01:39:22 +0000

    API working

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import gensim
 from gensim.models import Word2Vec
-
-BANNED_CHARACTERS = "\"!@#$%^&.`*()-+?_=,<>/123456789\'"
-EMPTY_WORD = "___________"
+from flask import *
+import json
 
 model = gensim.models.KeyedVectors.load_word2vec_format('venv/data/GoogleNews-vectors-negative300.bin', binary=True)
 
-sim_list = model.most_similar(positive="chelsea")
+app = Flask(__name__)
 
-print(sim_list)
+
+@app.route('/request/', methods=['GET'])
+def request_page():
+    words_query = str(request.args.get('user_words')) # localhost:7777/request?user_words=word1,word2,word3
+    word_list = words_query.split(',')
+
+    sim_list = model.most_similar(positive=word_list, topn=10000)
+
+    json_dump = json.dumps(sim_list)
+
+    return json_dump
+
+
+app.run(port=7777)
